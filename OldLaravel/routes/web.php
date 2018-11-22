aa75f9b5356fe26531995cd0941950e932e105be
h.takeda <h.takeda.solacom@gmail.com>
2018-11-22 16:21:42 +0900

    挨拶アプリ作成

--- OldLaravel/routes/web.php
+++ OldLaravel/routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello','HelloController@index');
+Route::post('/hello','HelloController@index');
+Route::view('/form','form');
